964a5d9e64256f8d407222e71636fc87b941024d
gou <gou@portworx.com>
2023-08-10 00:07:58 +0000

    fix txt chat app

--- chattxt.py
+++ chattxt.py
@@ -5,9 +5,18 @@ from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.document_loaders import DirectoryLoader
 
-# 1: add API key as environment variable
-#API_KEY = env('OPENAI_API_KEY')
-os.environ["OPENAI_API_KEY"] = "redacted"
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
+OPENAI_API_KEY = env('OPENAI_API_KEY')
+
+if OPENAI_API_KEY == "":
+	print("Missing OpenAPI key")
+	exit()
+
+print("Using OpenAPI with key ["+OPENAI_API_KEY+"]")
+
 
 # 2: load your data document
 #loader = TextLoader("data.txt")
@@ -19,8 +28,25 @@ loader = DirectoryLoader("datasets/px", glob="*.pdf")
 index = VectorstoreIndexCreator().from_loaders([loader])
 
 # 4: get prompt specified via command line
-query = sys.argv[1]
+#query = sys.argv[1]
 
 # 5: query the index
 # 6: output the response
-print(index.query(query))
+#print(index.query(query))
+
+def get_prompt():
+	print("Type 'exit' to quit")
+
+	while True:
+		prompt = input("Enter a prompt: ")
+
+		if prompt.lower() == 'exit':
+			print('Exiting...')
+			break
+		else:
+			try:
+				print(index.query(prompt))
+			except Exception as e:
+				print(e)
+
+get_prompt()
