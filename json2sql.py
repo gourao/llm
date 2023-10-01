b8308865101470511609d390db6e5a52c74567eb
gou <gou@portworx.com>
2023-10-01 17:30:02 +0000

    raw chat

--- json2sql.py
+++ json2sql.py
@@ -18,34 +18,35 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
+if len(sys.argv) < 3:
+	print("Missing  args.  Example 'json2sql schema.json <path to data> postgresql+psycopg2://postgres:1234@localhost:6667/mydb'")
+	exit()
 
 def trim_(input_string):
-	last_underscore_index = input_string.rfind("_")
+	last_underscore_index = input_string.rfind("_\d+")
 	if last_underscore_index != -1:  # If underscore is found
 		return input_string[:last_underscore_index]
 	else:
 		return ""
-
-if len(sys.argv) < 3:
-	print("Missing  args.  Example 'json2sql schema.json <path to data> postgresql+psycopg2://postgres:1234@localhost:6667/mydb'")
-	exit()
 
 # Breath first so the parent tables get created before children
 def create_schema(cursor, schema, table_name=''):
 	parent_table_name = trim_(table_name)
 	print("Processing JSON for : ", table_name, parent_table_name)
 
-	# input(schema)
-
 	column_types = {}
+	child_schemas = {}
+	child_id = 0
 
 	if isinstance(schema, dict):
 		# If JSON data is a dictionary, recursively process its values
 		for key, value in schema.items():
-			key = re.sub(r'[^a-zA-Z0-9_]', '_', key).lower()
+			key = re.sub(r'[^a-zA-Z0-9]', '_', key).lower()
+
 			if isinstance(value, (dict, list)):
 				# For nested dictionaries create a separate table with the key in the table name
-				create_schema(cursor, value, table_name + "_" + key)
+				child_schemas[table_name + "_" + str(child_id) + "_" + key] = value
+				child_id = child_id + 1
 			else:
 				# Insert primitive values as a column
 				if value == "int":
@@ -61,38 +62,43 @@ def create_schema(cursor, schema, table_name=''):
 		for item in schema:
 			if isinstance(item, (dict, list)):
 				# For nested lists, create a separate table with a 0 in the table name to indicate a list
-				create_schema(cursor, item, table_name + "_0")
+				child_schemas[table_name + "_" + str(child_id)] = item
+				child_id = child_id + 1
 			else:
 				# Insert primitive values as a column
-				item = re.sub(r'[^a-zA-Z0-9_]', '_', item).lower()
+				item = re.sub(r'[^a-zA-Z0-9]', '_', item).lower()
+
 				if value == "int":
-					column_types[key] = 'INTEGER'
+					column_types[item] = 'INTEGER'
 				elif value == "float":
-					column_types[key] = 'REAL'
+					column_types[item] = 'REAL'
 				elif value == "bool":
-					column_types[key] = 'BOOL'
+					column_types[item] = 'BOOL'
 				else:
-					column_types[key] = 'TEXT'
+					column_types[item] = 'TEXT'
 
 	# Create SQL table if there are any primitive columns to add
-	if len(column_types) > 0:
-		drop_table_sql = f"DROP TABLE IF EXISTS {table_name}"
-		cursor.execute(drop_table_sql)
+	drop_table_sql = f"DROP TABLE IF EXISTS {table_name} CASCADE;"
+	cursor.execute(drop_table_sql)
 
-		# Needed for GPT-3.5... override any existing ID field in the 
-		# original data so we can create a relationship
-		if column_types.get("id") is not None:
-			print("Overriding existing ID of type ", column_types["id"])
-	
-		column_types["id"] = 'INTEGER PRIMARY KEY'
-		create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} ("
-		create_table_sql += ", ".join([f"{key} {column_types[key]}" for key in column_types])
-		if parent_table_name != "" :
-			create_table_sql += f", FOREIGN KEY (id) REFERENCES {parent_table_name}(id)"
-		create_table_sql += ");"
+	# Needed for GPT-3.5... override any existing ID field in the 
+	# original data so we can create a relationship
+	if column_types.get("id") is not None:
+		print("Overriding existing ID of type ", column_types["id"])
+	column_types["id"] = 'INTEGER PRIMARY KEY'
 
-		print("Executing: ", create_table_sql)
-		cursor.execute(create_table_sql)
+	create_table_sql = f"CREATE TABLE {table_name} ("
+	create_table_sql += ", ".join([f"{key} {column_types[key]}" for key in column_types])
+	if parent_table_name != "" :
+		create_table_sql += f", FOREIGN KEY (id) REFERENCES {parent_table_name}(id)"
+	create_table_sql += ");"
+
+	print("Executing: ", create_table_sql)
+	cursor.execute(create_table_sql)
+
+	# Create child schemas
+	for key, value in child_schemas.items():
+		create_schema(cursor, value, key)
 
 def import_data(cursor, json_data_path):
 	pass
